28c8feaf8237dea8df43e0519eb563364955f918
chaitanya7666 <chaitu.y25@gmail.com>
2021-08-08 19:34:06 +0530

    added the delete item functionality

--- src/App.js
+++ src/App.js
@@ -1,139 +1,115 @@
 import {
   Button,
-  Checkbox,
   FormHelperText,
   ListItem,
-  ListItemIcon,
-  ListItemText,
-  makeStyles,
   TextField,
+  ListItemText,
 } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
-import React, { useState, useEffect } from "react";
-
+import React, { Component } from "react";
 import db from "./firebase";
-import firebase from "firebase";
+import firebase from "firebase/app";
 
-function App() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      width: "100%",
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }));
-
-  const [todo, setTodo] = useState([]);
-  const [input, setInput] = useState("");
-  const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
-
-  useEffect(() => {
-    async function getData() {
-      const { docs: dbCollection } = await await db.collection("todos").get();
-      const todos = [];
-      dbCollection.forEach((doc) => todos.push(doc.data()["todo"]));
-      // console.log(todos);
-      setTodo([...todos]);
-    }
-    getData();
-  });
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
+class App extends Component {
+  state = {
+    todo: [],
+    input: "",
   };
 
-  return (
-    <div>
-      <h1>App component</h1>
+  async componentDidMount() {
+    const { docs: dbCollection } = await db
+      .collection("todos")
+      .orderBy("timestamp", "desc")
+      .get();
+    const todo = [...dbCollection];
+    console.log("cdm", todo);
 
-      <form
-        action="Submit"
-        onSubmit={(e) => {
-          db.collection("todos").add({
-            todo: input,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-          });
-          // setTodo([...todo, input]);
-          setInput("");
-          e.preventDefault();
-        }}
-      >
-        <TextField
-          id="outlined-basic"
-          label="Enter here"
-          variant="outlined"
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          style={{ marginRight: 10 }}
-        />
+    this.setState({ todo });
+  }
 
-        <FormHelperText id="my-helper-text">
-          Prioritise your Todo list carefully!
-        </FormHelperText>
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("input is : ", this.state.input);
 
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            db.collection("todos").add({
-              todo: input,
-              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-            });
+    await db.collection("todos").add({
+      todo: this.state.input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
 
-            // setTodo([...todo, input]);
-            setInput("");
-          }}
-          disabled={!input}
-        >
-          Add to List
-        </Button>
-      </form>
+    const { docs: dbCollection } = await db
+      .collection("todos")
+      .orderBy("timestamp", "desc")
+      .get();
+    const todo = [...dbCollection];
+    const input = "";
+    this.setState({ todo, input });
 
+    console.log("handle subm");
+  };
+
+  handleInputChange = (e) => {
+    const input = e.target.value;
+    this.setState({ input });
+  };
+
+  handleDelete = async (item) => {
+    await db.collection("todos").doc(item.id).delete();
+    const { docs: dbCollection } = await db
+      .collection("todos")
+      .orderBy("timestamp", "desc")
+      .get();
+    const todo = [...dbCollection];
+    this.setState({ todo });
+  };
+
+  render() {
+    const { todo, input } = this.state;
+    return (
       <div>
-        <ul>
-          {todo.map((item) => (
-            <div>
-              <ListItem
-                key={item}
-                role={undefined}
-                dense
-                button
-                onClick={handleToggle(item)}
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(item) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ "aria-labelledby": item }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={item} primary={item} />
-                <Button variant="contained" color="secondary">
-                  DELETE
-                </Button>
-                <Button variant="contained" color="primary">
-                  CHECK/UNCHECK
-                </Button>
-              </ListItem>
-            </div>
-          ))}
-        </ul>
+        <h1>Todo List Application</h1>
+
+        <form action="Submit" onSubmit={(e) => this.handleSubmit(e)}>
+          <TextField
+            id="outlined-basic"
+            label="Enter here"
+            variant="outlined"
+            type="text"
+            value={this.state.input}
+            onChange={(e) => this.handleInputChange(e)}
+            style={{ marginRight: 10 }}
+          />
+
+          <FormHelperText id="my-helper-text">
+            Prioritise your Todo list carefully!
+          </FormHelperText>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={(e) => this.handleSubmit(e)}
+            disabled={!input}
+          >
+            Add to List
+          </Button>
+        </form>
+
+        <div>
+          <ul>
+            {todo.map((item) => (
+              <div key={item.id}>
+                <ListItem role={undefined} dense button>
+                  <ListItemText primary={item.data().todo} />
+
+                  <DeleteForeverIcon onClick={() => this.handleDelete(item)} />
+                </ListItem>
+              </div>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
